ec4c0c1a80d0ef2f66077ccb4b68234e10a70f0b
jameshub97 <jamesalexcarr@outlook.com>
2026-04-01 12:16:59 +0100

    test

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
 </script>
 
 <template>
@@ -8,7 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hello world" />
+      <HelloWorld msg="Hello test" />
+      <HelloWorld2 msg="Hello teffesst" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
